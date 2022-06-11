b422f6421766fedea0f634f5d3e70f42b4a94056
D.Dean.Draper <wv6oyo@gmail.com>
2022-06-11 21:22:24 +0800

    exploring js, dom.enum() ...

--- float/script.js
+++ float/script.js
@@ -1,34 +1,51 @@
 //
 //
 //
-//
-//
-//
-//
 let footer;
+
+let dom = {
+    tags: null,
+    enum: function(){
+	this.tags = document.getElementsByTagName("*");
+	// an HTMLCollection
+    },
+    named: function(id){
+	return this.tags.namedItem(id);
+    },
+    show: function(){
+	for(i=0; i<this.tags.length; i++){
+	    console.log("element=>",this.tags.item(i))
+	}
+    }
+}
 
 /// DOMContentLoaded
 document.addEventListener("DOMContentLoaded", function(event) { // Wait for DOM to load
     console.log(event);
-    footer = document.getElementsByClassName("footer")[0];
-    x = document.getElementsByTagName("*");
-    console.log("DOMelements=>",x.length);
-    //    footer.innerHTML += "<p>"
-    let s = "";
-    for (i=0; i<x.length; i++){
-	console.log("elementID=>",x[i].id,"/",x[i].tagName);
-	s += "elementID=>" + x[i].id + "/" + x[i].tagName + "__:__"
-    }
-    footer.innerHTML = `<p>${s}</p>`
+    //footer = document.getElementById("footer")[0];
+    dom.enum();
+    dom.show();
     initialize();
 });
 
+
+
 function initialize(){
     console.log("bingo");
-    footer = document.getElementsByClassName("footer");
-    content = document.getElementsByClassName("content")[0];
+    footer = document.getElementById("footer");
+    content = document.getElementById("content");
     content.innerHTML += "<br>blue  => html"
     content.innerHTML += "<br>black => body"
     content.innerHTML += "<br>red   => .page"
-	
+
+    dom.named("body").onclick = handleClick;
+}
+
+
+
+function handleClick(e){
+    console.log(e);
+    x = e.target;
+    footer.innerHTML = `target=/${x.id}/${x.tagName}/`;
+    //console.log(e.target.tagName);
 }
